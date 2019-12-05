238115c02dcd72d1b0cfeda229c23fa8fc4e60fa
Sergey Kirpik <sergeykirpik@gmail.com>
2019-12-05 08:15:58 +0200

    Updates

---
+++ precache-manifest.3c4d1ca384110cfcd123145851309519.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9400f295d99ee30fe36302281f2623f8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1f75d3c823720dd753a7",
+    "url": "/monsters-rolodex/static/css/main.18b5a6e6.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "1f75d3c823720dd753a7",
+    "url": "/monsters-rolodex/static/js/main.6751bc2c.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
